d89d46d90c7c44d26007576ec960314add5b8c46
James Bean <jamesbean@Jamess-MacBook-Pro.local>
2019-07-09 14:40:31 -0700

    Add failing enum with associated values test

---
+++ Tests/XMLCoderTests/EnumAssociatedValueTest.swift
@@ -0,0 +1,50 @@
+//
+//  EnumAssociatedValueTest.swift
+//  XMLCoderTests
+//
+//  Created by James Bean on 7/9/19.
+//
+
+import XCTest
+import XMLCoder
+
+enum IntOrString {
+    case int(Int)
+    case string(String)
+}
+
+extension IntOrString: Decodable {
+    
+    enum CodingKeys: String, CodingKey {
+        case int
+        case string
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        do {
+            self = .int(try container.decode(Int.self, forKey: .int))
+        } catch {
+            self = .string(try container.decode(String.self, forKey: .string))
+        }
+    }
+}
+
+extension IntOrString: Equatable { }
+
+class EnumAssociatedValuesTest: XCTestCase {
+    
+    func testIntOrStringIntDecoding() throws {
+        let xml = "<int>42</int>"
+        let result = try XMLDecoder().decode(IntOrString.self, from: xml.data(using: .utf8)!)
+        let expected = IntOrString.int(42)
+        XCTAssertEqual(result, expected)
+    }
+    
+    func testIntOrStringStringDecoding() throws {
+        let xml = "<string>forty-two</string>"
+        let result = try XMLDecoder().decode(IntOrString.self, from: xml.data(using: .utf8)!)
+        let expected = IntOrString.string("forty-two")
+        XCTAssertEqual(result, expected)
+    }
+}
